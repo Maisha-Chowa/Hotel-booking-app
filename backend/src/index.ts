721e1ec81ea66190d0d5b23ac8ba1f418d3f8013
Maisha-Chowa <maisha.chowa@gmail.com>
2024-03-16 20:17:36 +0600

    fixing-bug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
-import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import myHotelRoutes from "./routes/my-hotels";
 import cookieParser from "cookie-parser";
 import path from "path";
 import { v2 as cloudinary } from "cloudinary";
+import mongoose from "mongoose";
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
